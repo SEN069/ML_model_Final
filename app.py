f573d2e10311dd914964b79a3a636c5bdfed7984
Suppreo Sen <sensuppreo@gmail.com>
2026-04-22 18:06:17 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+import numpy as np
+
+def predict_diabetes(preg, glucose, bp, skin, insulin, bmi, dpf, age):
+    input_data = np.array([[preg, glucose, bp, skin, insulin, bmi, dpf, age]])
+    prediction = best_model.predict(input_data)
+
+    return "Diabetic" if prediction[0] == 1 else "Not Diabetic"
+
+interface = gr.Interface(
+    fn=predict_diabetes,
+    inputs=[
+        "number","number","number","number",
+        "number","number","number","number"
+    ],
+    outputs="text",
+    title="Diabetes Prediction App"
+)
+
+interface.launch()
---
+++ main.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+import joblib
+
+df = pd.read_csv("diabetes.csv")
+
+
+cols_with_zero = ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']
+df[cols_with_zero] = df[cols_with_zero].replace(0, np.nan)
+
+# Fill Missing Values
+df.fillna(df.median(), inplace=True)
+
+scaler = StandardScaler()
+X = df.drop("Outcome", axis=1)
+y = df["Outcome"]
+
+X_scaled = scaler.fit_transform(X)
+
+#Train-test split
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42
+)
+
+# Simple clipping
+df = df.clip(lower=df.quantile(0.01), upper=df.quantile(0.99), axis=1)
+
+pipeline = Pipeline([
+    ('scaler', StandardScaler()),
+    ('model', RandomForestClassifier(random_state=42))
+])
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(pipeline, X_scaled, y, cv=5)
+
+print("CV Mean:", scores.mean())
+print("CV Std:", scores.std())
+
+
+param_grid = {
+    'model__n_estimators': [100, 200],
+    'model__max_depth': [None, 5, 10],
+    'model__min_samples_split': [2, 5],
+}
+
+grid = GridSearchCV(pipeline, param_grid, cv=5)
+grid.fit(X_train, y_train)
+
+print("Best Params:", grid.best_params_)
+
+best_model = grid.best_estimator_
+
+
+y_pred = best_model.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n", classification_report(y_test, y_pred))
+print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
